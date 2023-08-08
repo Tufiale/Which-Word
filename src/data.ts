61beb456fb0b705af44a796f0f5ff5c7d5c8d510
Tufiale <tufiale@gmail.com>
2023-08-08 15:57:40 +0100

    Added opject & type with functions and event listn

--- src/data.ts
+++ src/data.ts
@@ -1,1 +1,59 @@
 // To store objects
+type QuizQuestion = {
+  question: string;
+  options: string[];
+  answer: number;
+};
+
+export const quizQuestions: QuizQuestion[] = [
+  {
+    question: "What is the scientific name for the 'fear of long words'?",
+    options: [
+      "Hippopotomonstrosesquippedaliophobia",
+      "Arachnophobia",
+      "Acrophobia",
+      "Claustrophobia",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Which element has the chemical symbol 'Pb'?",
+    options: ["Platinum", "Phosphorus", "Palladium", "Lead"],
+    answer: 3,
+  },
+  {
+    question: "Who was the first woman to win a Nobel Prize?",
+    options: [
+      "Marie Curie",
+      "Rosa Parks",
+      "Florence Nightingale",
+      "Amelia Earhart",
+    ],
+    answer: 0,
+  },
+  {
+    question: "What is the rarest M&M color?",
+    options: ["Red", "Blue", "Brown", "Orange"],
+    answer: 2,
+  },
+  {
+    question: "What is the tallest mountain in Africa?",
+    options: ["Mount Everest", "Kilimanjaro", "Mount McKinley", "Mount Fuji"],
+    answer: 1,
+  },
+  {
+    question: "Which planet in our solar system has the most moons?",
+    options: ["Jupiter", "Saturn", "Neptune", "Uranus"],
+    answer: 0,
+  },
+  {
+    question: "What is the main ingredient in traditional Japanese miso soup?",
+    options: ["Seaweed", "Tofu", "Mushrooms", "Fish"],
+    answer: 1,
+  },
+  {
+    question: "In Greek mythology, who was the goddess of victory?",
+    options: ["Athena", "Hera", "Nike", "Artemis"],
+    answer: 2,
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
 import "./style.scss";
+import { quizQuestions } from "./data";
+
+let currentQuestionNumber = 0;
+// This will change over time^^ hence let variable
+
+// Query Selectors
+let currentQuestion = document.querySelector(".quiz__question") as HTMLElement;
+let currentAnswers =
+  document.querySelectorAll<HTMLButtonElement>(".answers__buttons");
+const nextBtn = document.querySelector(".next") as HTMLButtonElement;
+
+console.log(currentAnswers);
+
+// Functions
+const updateQuizText = () => {
+  currentQuestion.textContent = quizQuestions[currentQuestionNumber].question;
+  for (let index = 0; index < currentAnswers.length; index++) {
+    currentAnswers[index].textContent =
+      quizQuestions[currentQuestionNumber].options[index];
+  }
+};
+updateQuizText();
+
+const handleUpdateQuestion = () => {
+  currentQuestionNumber++;
+  updateQuizText();
+};
+
+//  Event Listeners
+
+nextBtn.addEventListener("click", handleUpdateQuestion);
