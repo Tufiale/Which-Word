1485b721beec87d7f1182b3ed7e08ccefa007180
Tufiale <tufiale@gmail.com>
2023-08-10 15:42:52 +0100

    Total score

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import "./style.scss";
 import { quizQuestions } from "./data";
 
-let currentQuestionNumber = 0;
-// This will change over time^^ hence let variable
-
 // Query Selectors
 let currentQuestion = document.querySelector(".quiz__question") as HTMLElement;
 let currentAnswers =
   document.querySelectorAll<HTMLButtonElement>(".answers__buttons");
 const nextBtn = document.querySelector(".next") as HTMLButtonElement;
+const score = document.querySelector(".footer__score") as HTMLHeadingElement;
+
+let currentQuestionNumber = 0;
+let correctScore = 0;
 
 // Functions
 const updateQuizText = () => {
@@ -31,19 +32,15 @@ const handleAnswer = (event: Event) => {
   const userAnswer = event.currentTarget.textContent;
   if (userAnswer === quizQuestions[currentQuestionNumber].answer) {
     event.currentTarget.style.backgroundColor = "green";
-    console.log(event);
+    correctScore++;
+    score.innerText = correctScore;
   } else {
     event.currentTarget.style.backgroundColor = "red";
   }
 };
 
-// Event Listener
+// Event Listeners
 nextBtn.addEventListener("click", handleUpdateQuestion);
 currentAnswers.forEach((answer) => {
   answer.addEventListener("click", handleAnswer);
 });
-
-// Next Button clears the red color and green color ---  DONE
-// Initial page needs to be a question
-// Stop watch
-// Maybe feature removing two answers
